c23ea63e7b46633f58b2bf88234ec0becc4043f8
ccordova <cristian@helmcode.com>
2024-04-25 14:47:12 +0100

    Added trusted component

---
+++ components/trusted.py
@@ -0,0 +1,40 @@
+import reflex as rx
+
+
+def company_logo(image: str) -> rx.Component:
+    return rx.image(
+        src=f"/{image}",
+        class_name="rounded-lg w-8 h-8"
+    )
+
+
+def trusted_component() -> rx.Component:
+    logos = [
+        "bbva.jpeg",
+        "telefonica.jpeg",
+        "ovhgroup.jpeg",
+        "glovo_app.jpeg",
+        "metricool.jpeg",
+        "paradigma_digital.jpeg",
+        "zinkee.jpeg",
+        "smartvel.jpeg",
+        "nowo_tech.jpeg",
+        "netmakers.jpeg"
+    ]
+
+    return rx.container(
+        rx.heading(
+            "TRUSTED BY",
+            size="1"
+        ),
+        rx.scroll_area(
+            rx.box(
+                *[
+                    company_logo(image)
+                    for image in logos
+                ],
+                class_name="flex gap-4"
+            )
+        ),
+        class_name="w-full text-center h-auto py-6 mb-4"
+    )
--- views/header.py
+++ views/header.py
@@ -1,6 +1,7 @@
 import reflex as rx
 from components.rrss import rrss_component
 from components.projects import project_component
+from components.trusted import trusted_component
 
 
 def header_view() -> rx.Component:
@@ -20,8 +21,9 @@ def header_view() -> rx.Component:
         ),
         rx.box(
             "Me dedico a ayudar a empresas con todo lo referente a su infraestructura Cloud. También voy contando cómo creo productos digitales en mis diferentes redes sociales ya que apoyo mucho el movimiento #buildinpublic.",
-            class_name="py-6 text-neutral-50 text-sm mb-4"
+            class_name="py-6 text-neutral-50 text-sm"
         ),
+        trusted_component(),
         rx.box(
             rx.heading("Proyectos"),
             project_component(),
